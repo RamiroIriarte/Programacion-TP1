e6ffe7db9607e08b74de7baa2dfdbb8afab6d696
Ramiro Iriarte <ramiro_iriarte003@hotmail.com>
2025-05-09 00:08:59 -0300

    se agrego index.html,style.cc,script.js

--- script.js
+++ script.js
@@ -1,22 +1,35 @@
-// Seleccionamos el <ul> donde mostraremos los usuarios
-const lista = document.getElementById('lista-usuarios');
+function buscarUniversidades() {
+  const input = document.getElementById("countryInput");
+  const country = input.value.trim();
+  const resultsList = document.getElementById("results");
+  resultsList.innerHTML = "";
 
-// Usamos fetch para hacer la solicitud GET a la API
-fetch('https://jsonplaceholder.typicode.com/users')
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Error en la respuesta de la API');
-    }
-    return response.json(); // Convertimos la respuesta a JSON
-  })
-  .then(data => {
-    // Recorremos el arreglo de usuarios y los mostramos
-    data.forEach(usuario => {
-      const li = document.createElement('li');
-      li.textContent = `${usuario.name} (${usuario.email})`;
-      lista.appendChild(li);
+  if (country === "") {
+    alert("Por favor, escribe un país.");
+    return;
+  }
+
+  const apiUrl =`http://universities.hipolabs.com/search?country=${encodeURIComponent(country)}`;
+
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      if (data.length === 0) {
+        resultsList.innerHTML = "<li>No se encontraron universidades para ese país.</li>";
+        return;
+      }
+
+      data.forEach(universidad => {
+        const li = document.createElement("li");
+        li.innerHTML = `
+          <strong class="destacado">${universidad.name}</strong><br>
+          <a href="${universidad.web_pages[0]}" class="enlace" target="_blank">${universidad.web_pages[0]}</a>
+        `;
+        resultsList.appendChild(li);
+      });
+    })
+    .catch(error => {
+      console.error("Error al obtener universidades:", error);
+      resultsList.innerHTML = "<li>Ocurrió un error al consultar la API.</li>";
     });
-  })
-  .catch(error => {
-    console.error('Hubo un problema:', error);
-  });
+}
